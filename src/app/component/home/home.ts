0ad2c10636ad857aadd4cbb0827c834b2746971e
aboze1n <midozean123@gmail.com>
2025-10-04 20:10:45 +0300

    API Key Applied

--- src/app/component/home/home.ts
+++ src/app/component/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Carousel } from "../carousel/carousel";
+import { MovieService } from '../../services/movie-service';
+import { IMovie } from '../../models/i-movie'; // استيراد الـ Interface
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,23 @@ import { Carousel } from "../carousel/carousel";
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
+  private movieService = inject(MovieService);
+  
+  // متغير عشان نخزن فيه قائمة الأفلام اللي هتيجي من الـ API
+  movies: IMovie[] = [];
 
+  ngOnInit(): void {
+    // أول ما الكومبوننت يشتغل، بنطلب الأفلام
+    this.movieService.getNowPlayingMovies().subscribe({
+      next: (response: any) => {
+        // الـ API بيرجع object اسمه results وجواه قائمة الأفلام
+        this.movies = response.results;
+        
+        // أهم خطوة: نتأكد إن الداتا وصلت عن طريق الـ console
+        console.log(this.movies); 
+      },
+      error: (err) => console.error('Error fetching movies:', err)
+    });
+  }
 }
--- src/app/services/movie-service.ts
+++ src/app/services/movie-service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private http = inject(HttpClient);
   
+  // !! مهم: غير "YOUR_API_KEY" بالـ Key الحقيقي بتاعك
+  private apiKey = 'dd724dfe8a2da5556091cf33e4f4d50d'; 
+  private baseUrl = 'https://api.themoviedb.org/3';
+
+  // دالة لجلب الأفلام المعروضة حالياً
+  getNowPlayingMovies(): Observable<any> {
+    const url = `${this.baseUrl}/movie/now_playing?api_key=${this.apiKey}`;
+    return this.http.get(url);
+  }
 }
