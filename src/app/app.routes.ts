eedbe3f3f54484fad2f6d22db5d13bf5e8d1d17f
Abdallahfarrag1 <frajbdallh751@gmail.com>
2025-10-05 11:22:51 +0300

    add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,16 @@ import { Wishlist } from './component/wishlist/wishlist';
 import { MovieDetail } from './component/movie-detail/movie-detail';
 import { MovieList } from './component/movie-list/movie-list';
 import { Carousel } from './component/carousel/carousel';
+import { Login } from './component/auth/login/login';
 
 export const routes: Routes = [
   { path: '', component: Home, title: 'Home' },
   { path: 'details/:id', component: MovieDetail, title: 'Movie Details' },
   { path: 'MovieList', component: MovieList, title: 'MovieList' },
+  {path:'login',component:Login,title:'login'},
   { path: 'Wishlist', component: Wishlist, title: 'Wishlist' },
   { path: 'Review', component: Review, title: 'Review' },
   { path: 'Carousel', component: Carousel, title: 'Carousel' },
-  
+
   { path: '**', component: NotFound, title: '404' },
 ];
