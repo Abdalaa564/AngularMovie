96914e41f258f5036b3961721fbe8e9dc4921eba
Abdalaa Khaleel <abdalaakhaleel564@gmail.com>
2025-10-10 18:17:54 +0300

    Delete src directory

--- src/app/loading-spinner/loading-spinner.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loading-spinner',
-  standalone: true,
-  templateUrl: './loading-spinner.html',
-  styleUrls: ['./loading-spinner.css']
-})
-export class LoadingSpinner {}
